f546fafd8d4394f97d8a6803e023b1973b256c29
juliantwx <juliantwx.dev@gmail.com>
2024-11-17 01:03:15 +1100

    Updated Routing
    - All URLs will now display the Maintenance page if the website is under maintenance.
    
    Usual Routing:
    - Base URL is set to the Home page.
    - All non existing routes will be met with the NotFound page.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,33 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import Maintenance from "./pages/Maintenance";
 
 function App() {
+  const isMaintenance = true; // TODO: Fetch this variable through subscription to the backend server instead
   return (
     <Router>
       <div className="min-h-screen">
         <Routes>
-          {/* Routes to all existing pages */}
-          <Route path="/" element={<Home />} />
-          <Route path="/maintenance" element={<Maintenance />} />
-          {/* Reroute all non-existing routes to the 404 page */}
-          <Route path="*" element={<NotFound />} />
+          {isMaintenance ? (
+            // Redirect all routes to the Maintenance page if the website is under maintenance
+            <Route path="*" element={<Maintenance />} />
+          ) : (
+            // Usual routing
+            <>
+              {/* Set default URL to the home page */}
+              <Route path="/" element={<Navigate to="/home" />} />
+              {/* Routes to all existing pages */}
+              <Route path="/home" element={<Home />} />
+              {/* Reroute all non-existing routes to the 404 page */}
+              <Route path="*" element={<NotFound />} />
+            </>
+          )}
         </Routes>
       </div>
     </Router>
