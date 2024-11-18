b58c5351d5fa3e8788bc2a05fe9f53787b21e235
juliantwx <juliantwx.dev@gmail.com>
2024-11-18 15:07:03 +1100

    Added slide down menu

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,11 +6,19 @@ import { useNavigate } from "react-router-dom";
 function Navbar() {
   const navigate = useNavigate();
   const menuItems = ["home", "projects", "updates"];
+  const [isHovering, setIsHovering] = useState(false);
   const [activeMenuItem, setActiveMenuItem] = useState(null);
 
   return (
-    <div className="fixed top-0 left-0 w-full bg-black p-4 z-1000 text-cream">
-      <div className="sm:px-5">
+    <div
+      className="relative"
+      onMouseEnter={() => {
+        setIsHovering(true);
+        console.log("isHovering");
+      }}
+      onMouseLeave={() => setIsHovering(false)}
+    >
+      <div className="fixed top-0 left-0 w-full bg-black text-cream py-4 px-4 sm:px-10 z-50">
         {/* Main Stack encapsulating all Navbar elements */}
         <Stack
           flexDirection="row"
@@ -52,7 +60,9 @@ function Navbar() {
                 <button
                   key={index}
                   className="transition-colors hover:text-amber"
-                  onClick={() => navigate(item)}
+                  // onClick={() => navigate(item)}
+                  onMouseEnter={() => setActiveMenuItem(item)}
+                  onMouseLeave={() => setActiveMenuItem(null)}
                 >
                   {item.toUpperCase()}
                 </button>
@@ -66,6 +76,14 @@ function Navbar() {
             CONTACT ME
           </button>
         </Stack>
+      </div>
+      {/* Navigation Menu that slides out on hover */}
+      <div
+        className={`transition-transform duration-500 ease-out fixed left-0 w-full bg-black text-cream p-4 z-40 ${
+          activeMenuItem ? "translate-y-16" : "-translate-y-full"
+        }`}
+      >
+        {`This is the slide out menu for ${activeMenuItem}`}
       </div>
     </div>
   );
