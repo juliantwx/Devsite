4f611a7b227bc58d141895791d2f660ff3505ae6
juliantwx <juliantwx.dev@gmail.com>
2025-02-08 01:31:27 +1100

    Adjusted Project Page Dimensions

--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -7,12 +7,12 @@ function Projects() {
   const [projects, setProjects] = useState(projectData);
 
   return (
-    <div className="min-h-inherit px-20 pt-32 sm:pt-40 pb-24">
+    <div className="min-h-inherit px-20 pt-32 sm:pt-40 pb-24 flex justify-center">
       <Grid2
         container
         columnSpacing={10}
         rowSpacing={7}
-        sx={{ width: "auto", justifyContent: "space-evenly" }}
+        sx={{ maxWidth: "1250px", justifyContent: "space-evenly" }}
       >
         {projects.map((project, index) => (
           <Grid2 item key={index}>
