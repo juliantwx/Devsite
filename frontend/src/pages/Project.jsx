1c48d12986e6836be30c3c5adeb4d988229aa909
juliantwx <juliantwx.dev@gmail.com>
2025-02-12 16:15:03 +1100

    Completed First Iteration of Project Page, Removed redundant code from Projects page

--- frontend/src/pages/Project.jsx
+++ frontend/src/pages/Project.jsx
@@ -2,20 +2,58 @@ import React, { useEffect, useState } from "react";
 import projects from "../data/projects";
 
 function Project() {
-  const [projectData, setProjectData] = useState(null);
+  const [project, setProject] = useState(null);
 
   useEffect(() => {
     // Retrieve project data based on path name
     const pathName = window.location.pathname;
-    const project = projects.find((p) => p.pageURL === pathName);
-
-    if (project) setProjectData(project);
+    setProject(projects.find((p) => p.pageURL === pathName));
   }, []);
 
   return (
-    <div className="min-h-inherit pt-32 sm:pt-40 pb-24 flex justify-center">
-      {projectData && projectData.name}
-      {projectData && projectData.desc}
+    <div className="min-h-inherit py-32 flex justify-center">
+      {project && (
+        <div className="flex justify-center flex-col w-[75%] sm:w-[50%]">
+          <div className="relative rounded-xl overflow-hidden">
+            <video
+              src={project.videoURL}
+              style={{
+                height: "100%",
+                width: project.videoWidth ?? "100%",
+                objectFit: "cover",
+              }}
+              autoPlay
+              loop
+              muted
+              playsInline
+              preload="auto"
+            />
+
+            {/* Image overlay, fades out on hover */}
+            <img
+              src={project.thumbnailURL}
+              alt={project.name}
+              style={{
+                position: "absolute",
+                height: "100%",
+                width: project.thumbnailWidth ?? "100%",
+                objectFit: "cover",
+                transition: "opacity 0.3s ease-in-out",
+                opacity: 1,
+              }}
+            />
+          </div>
+          <div className="my-7">
+            <h1 className="text-5xl">{project.name}</h1>
+            <p className="mx-1 italic opacity-[0.7]">{project.type}</p>
+            <p className="mx-1 italic opacity-[0.7]">
+              {project.tags.join(", ")}
+            </p>
+          </div>
+          <p className="text-lg text-justify">{project.desc}</p>
+          <div></div>
+        </div>
+      )}
     </div>
   );
 }
--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -1,11 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import { Grid2 } from "@mui/material";
 import ProjectPanel from "../components/ProjectPanel";
-import projectData from "../data/projects";
+import projects from "../data/projects";
 
 function Projects() {
-  const [projects, setProjects] = useState(projectData);
-
   return (
     <div className="min-h-inherit pt-32 sm:pt-40 pb-24 flex justify-center">
       <Grid2
